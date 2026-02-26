4c4f101864453a35a3f786e73f55b5a80dfa9db6
Gui-mpereira <202507139908@alunos.unimetrocamp.edu.br>
2026-02-26 13:02:52 -0300

    adicionadas classes de secutiry

---
+++ Usuario/src/main/java/Infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package Infrastructure.repository;
+
+import com.gui_mpereira.aprendendo_spring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone,Long> {
+}
